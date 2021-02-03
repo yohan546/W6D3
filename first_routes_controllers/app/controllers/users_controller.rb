bb5de54b954aaf1414f67a163428535577157c56
Lijun <gan.lijun.glj@gmail.com>
2021-02-03 17:06:40 -0500

    create users table, model and add data through postman

--- first_routes_controllers/app/controllers/users_controller.rb
+++ first_routes_controllers/app/controllers/users_controller.rb
@@ -1,17 +1,21 @@
 class UsersController < ApplicationController 
     def index 
-        # users = User.all
-        # render json: users
-        render plain: "I'm in the index action!"
+        users = User.all
+        render json: users
+        # render plain: "I'm in the index action!"
     end
 
     def create 
-
-        render json: params
+        # render json: params
+        user = User.new(params.require(:user).permit(:name,:email))
+        # replace the `user_attributes_here` with the actual attribute keys
+        user.save!
+        render json: user
     end
 
     def show 
-
+        # user = User.find(params[:id])
+        # render json: user
         render json: params
     end
 end
