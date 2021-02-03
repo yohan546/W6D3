dbed8c879baba94f424235ba9cdbd670a099bb59
yohan oh <yohan.oh332@gmail.com>
2021-02-03 14:10:58 -0500

    started project

---
+++ first_routes_controllers/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :users
+
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/new', to: 'users#new', as: 'new_user'
+end
